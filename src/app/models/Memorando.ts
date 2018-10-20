c00bcc104d73f5ecce2960d4b021803810160891
arthur007110 <arthurcosta007110@gmail.com>
2018-10-19 21:14:42 -0300

    salve

--- src/app/models/Memorando.ts
+++ src/app/models/Memorando.ts
@@ -1,8 +1,10 @@
+import {Setor} from "src/app/models/Setor";
+
 export class Memorando{
     mensagem:String;
     dataEnvio:Date;
     dataChegada:Date;
     setorEmissor:Setor;
     setorDestinatario:Setor;
-    
+  
 }
