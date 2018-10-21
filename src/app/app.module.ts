29568b23c27b4eb9577b6a2794ad66ad48a4afae
arthur007110 <arthurcosta007110@gmail.com>
2018-10-21 20:38:06 -0300

    pode dar erro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import{RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { CadastroComponent } from './cadastro-usuario/cadastro.component';
+import { CadastroSetorComponent } from './cadastro-setor/cadastro-setor.component';
+import { EnvioMemorandoComponent } from './envio-memorando/envio-memorando.component';
+import { AtualizarMemorandosComponent } from './atualizar-memorandos/atualizar-memorandos.component';
+import { HomeComponent } from './home/home.component';
 
-import {FormsModule} from '@angular/forms';
-import{RouterModule, Routes} from '@angular/router';
+
+import { UsuarioService } from './serviços/usuario.service';
 
 import {PasswordModule} from 'primeng/password';
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import {InputMaskModule} from 'primeng/inputmask';
 import {CardModule} from 'primeng/card';
-import { HomeComponent } from './home/home.component';
 import {MenubarModule} from 'primeng/menubar';
 import {SidebarModule} from 'primeng/sidebar';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 
-import { CadastroSetorComponent } from './cadastro-setor/cadastro-setor.component';
-import { EnvioMemorandoComponent } from './envio-memorando/envio-memorando.component';
-import { UsuarioService } from './serviços/usuario.service;
-import { AtualizarMemorandosComponent } from './atualizar-memorandos/atualizar-memorandos.component'';
+
+
 
 export const rotas: Routes = [
   { path: 'cadastro', component: CadastroComponent },
@@ -38,8 +41,8 @@ export const rotas: Routes = [
     CadastroComponent,
     HomeComponent,
     CadastroSetorComponent,
-    EnvioMemorandoComponen,
-    AtualizarMemorandosComponentt
+    EnvioMemorandoComponent,
+    AtualizarMemorandosComponent
   ],
   imports: [
     BrowserModule,
--- src/app/cadastro-setor/cadastro-setor.component.ts
+++ src/app/cadastro-setor/cadastro-setor.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-<<<<<<< HEAD
-import { DadosService } from '../dados.service';
-=======
->>>>>>> 6f3b70a9244a10574355c618ffb832593568fec0
 
 @Component({
   selector: 'app-cadastro-setor',
@@ -11,30 +7,19 @@ import { DadosService } from '../dados.service';
   styleUrls: ['./cadastro-setor.component.css']
 })
 export class CadastroSetorComponent implements OnInit {
-<<<<<<< HEAD
-  nome: string;
-  id: string;
-  constructor(private router: Router, private dadosService: DadosService) { }
-=======
 
   constructor(private router: Router) { }
 
   siape:string;
->>>>>>> 6f3b70a9244a10574355c618ffb832593568fec0
 
   ngOnInit() {
     this.siape=sessionStorage.getItem("siape");
   }
-<<<<<<< HEAD
-  irParaTelaDeSetores(){
-    this.router.navigate(["/setores"])
-=======
 
   cadastrar(){
 
   }
   irParaTelaHome(){
     this.router.navigate(['/home',this.siape]);
->>>>>>> 6f3b70a9244a10574355c618ffb832593568fec0
   }
 }
