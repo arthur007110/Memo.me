a2532fba5460ee9d57efa63a9d1f55a9e94b2f3c
Gabriel <josegabrielpereira2017@gmail.com>
2018-11-06 21:24:20 -0200

    Firebase

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const configuracao = {
+    apiKey: "AIzaSyCeReNdzr4PDSLHq8tM-EYZbm6KRqy7YKk",
+    authDomain: "sistema-memorandos.firebaseapp.com",
+    databaseURL: "https://sistema-memorandos.firebaseio.com",
+    projectId: "sistema-memorandos",
+    storageBucket: "sistema-memorandos.appspot.com",
+    messagingSenderId: "71554009323"
+};
