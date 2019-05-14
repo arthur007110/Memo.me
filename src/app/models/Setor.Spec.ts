ed8c6f6bc2bb04d309da9dcfba8261271ce97ea5
EgoistaGentil <44187362+EgoistaGentil@users.noreply.github.com>
2019-05-14 20:13:33 -0300

    Teste Models
    Apagados os demais Specs e add somente os models tests

---
+++ src/app/models/Setor.Spec.ts
@@ -0,0 +1,6 @@
+import { Setor } from '../models/Setor';
+describe("Testes referentes à Setor.ts.", function(){
+  it("Deve verificar se todos os campos estão preenchidos corretamente", function(){
+    expect(object.verificarCampos())toBeFalsy();
+  });
+});
---
+++ src/app/models/Usuario.Spec.ts
@@ -0,0 +1,6 @@
+import { Usuario } from '../models/Usuario';
+describe("Testes referentes à Usuario.ts",function(){
+  it("Deve testar o método verificarCampos", function(){
+    expect(object.verificarCampos())toBeFalsy();
+  });
+});
